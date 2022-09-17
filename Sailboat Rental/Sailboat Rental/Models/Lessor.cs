a0444412a08e3928017779cba6ce592aa74be1af
Ahoora Bagheri <janalizade@gmail.com>
2022-09-17 13:12:09 +0200

    convert C# data types to Mongo Data type

--- Sailboat Rental/Sailboat Rental/Models/Lessor.cs
+++ Sailboat Rental/Sailboat Rental/Models/Lessor.cs
@@ -2,13 +2,16 @@
 using MongoDB.Bson.Serialization.Attributes;
 namespace Sailboat_Rental.Models
 {
-    public class Category
+    public class Lessor
     {
         // Convert C# Datatype object to MongoDb Datatype object and vice versa 
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
-        public string CategoryId { get; set; } = string.Empty;
-        [BsonElement("categoryname")]
-        public string CategoryName { get; set; } = string.Empty;
+        public string Id { get; set; }=String.Empty;
+        [BsonElement("name")]
+        public string Name { get; set; }=String.Empty;
+        [BsonElement("address")]
+        public string Address { get; set; }=String.Empty;
+
     }
 }
