45f46885f1ac2e46e82ea5c58594091669e8b1a1
Ahoora Bagheri <janalizade@gmail.com>
2022-09-17 15:46:19 +0200

    Add Mongo Driver and category Services

---
+++ SailBoat-Rental/SailBoat-Rental/Services/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using Sailboat_Rental.Models;
+namespace SailBoat_Rental.Services
+{
+    public interface ICategoryService
+    {
+        List<Category> GetCategories();
+        Category GetCategory(string id);       
+        Category GetCategoryById(string id);   
+        Category Create(Category category);
+        void Update(Category category);
+        void Delete(int id);
+    }
+}
